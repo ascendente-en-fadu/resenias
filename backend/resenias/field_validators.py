86feb1d739baba7e76bdee7e4b84ded53d3c1ccf
Guido Verna Russo <dogui@pop-os.localdomain>
2023-01-06 19:13:46 -0300

    agrego modelos con migracion para la DB

---
+++ backend/resenias/field_validators.py
@@ -0,0 +1,16 @@
+"""Validators para los models fields de la aplicacion resenias"""
+import datetime
+from django.core.exceptions import ValidationError
+ANIO_MINIMO = 2010
+
+
+def validar_anio(value):
+    """Valida que el anio este entre 2010 y el anio actual (incluidos)."""
+    if value > datetime.date.today().year:
+        raise ValidationError(
+            message="No puede ingresar un año que aún no haya transcurrido"
+        )
+    elif value < ANIO_MINIMO:
+        raise ValidationError(
+            message=f'El año debe ser superior a {ANIO_MINIMO}'
+        )
--- backend/resenias/models.py
+++ backend/resenias/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from resenias.field_validators import validar_anio
+
 
 # Create your models here.
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=255)
+    carrera = models.ForeignKey(
+        Carrera, on_delete=models.PROTECT,
+        related_name="materias", related_query_name="materia",
+    )
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Catedra(models.Model):
+    nombre = models.CharField(max_length=255)
+    materia = models.ForeignKey(
+        Materia, on_delete=models.PROTECT,
+        related_name="catedras", related_query_name="catedra",
+    )
+    turno_maniana = models.BooleanField(
+        verbose_name="turno mañana",
+        help_text="Se imparte en el turno mañana."
+    )
+    turno_tarde = models.BooleanField(
+        help_text="Se imparte en el turno tarde."
+    )
+    turno_noche = models.BooleanField(
+        help_text="Se imparte en el turno noche."
+    )
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Resenia(models.Model):
+    BAJO_NIVEL = 0
+    NIVEL = 100000
+    SOBRE_NIVEL = 200000
+
+    CALIFICACION_CHOICES = (
+        (BAJO_NIVEL, 'Bajo nivel'),
+        (NIVEL, 'Nivel'),
+        (SOBRE_NIVEL, 'Sobre nivel'),
+    )
+
+    catedra = models.ForeignKey(
+        Catedra, on_delete=models.PROTECT,
+        related_name="resenias", related_query_name="resenia",
+    )
+
+    # autor (para hacer con google login)
+    calificacion = models.PositiveIntegerField(
+        verbose_name="calificación", choices=CALIFICACION_CHOICES,
+        blank=True, null=True,
+    )
+    contenido = models.TextField(help_text="Escriba su reseña en este campo.")
+    anio = models.PositiveIntegerField(
+        verbose_name="año de cursada", validators=[validar_anio]
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "reseña"
