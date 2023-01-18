61212706a12a51028e61d271a4a0ece9baa2ed3a
dev-guido <guidovernarusso@gmail.com>
2023-01-18 20:40:18 -0300

    se crean modelos y se agregan al sitio administrativo

--- backend/resenias/admin.py
+++ backend/resenias/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
+
+
+class CarreraAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+
+class MateriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'carrera')
+
+
+class CatedraAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre', 'materia', 'turno_maniana', 'turno_tarde', 'turno_noche'
+    )
+
+
+class ReseniaAdmin(admin.ModelAdmin):
+    list_display = ('catedra', 'calificacion', 'anio', 'created', 'updated')
+
 
 # Register your models here.
+admin.site.register(Carrera, CarreraAdmin)
+admin.site.register(Materia, MateriaAdmin)
+admin.site.register(Catedra, CatedraAdmin)
+admin.site.register(Resenia, ReseniaAdmin)
