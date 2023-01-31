b847e0d118f3422d7f333b69ec0cdcefc01ce8bb
dev-guido <guidovernarusso@gmail.com>
2023-01-31 17:32:20 -0300

    agrego posibilidad de filtrar entidades segun ancestro

--- backend/resenias/views.py
+++ backend/resenias/views.py
@@ -11,14 +11,44 @@ class CarreraView(viewsets.ModelViewSet):
 
 class MateriaView(viewsets.ModelViewSet):
     serializer_class = MateriaSerializer
-    queryset = Materia.objects.all()
+
+    def get_queryset(self):
+        """
+        Si se especifica de que carrera deben ser
+        las materias las filtra con este criterio.
+        """
+        queryset = Materia.objects.all()
+        carrera = self.request.query_params.get('carrera')
+        if carrera is not None:
+            queryset = queryset.filter(carrera=carrera)
+        return queryset
 
 
 class CatedraView(viewsets.ModelViewSet):
     serializer_class = CatedraSerializer
-    queryset = Catedra.objects.all()
+
+    def get_queryset(self):
+        """
+        Si se especifica de que materia deben ser
+        las catedras las filtra con este criterio.
+        """
+        queryset = Catedra.objects.all()
+        materia = self.request.query_params.get('materia')
+        if materia is not None:
+            queryset = queryset.filter(materia=materia)
+        return queryset
 
 
 class ReseniaView(viewsets.ModelViewSet):
     serializer_class = ReseniaSerializer
-    queryset = Resenia.objects.all()
+
+    def get_queryset(self):
+        """
+        Si se especifica de que catedra deben ser
+        las resenias las filtra con este criterio.
+        """
+        queryset = Resenia.objects.all()
+        catedra = self.request.query_params.get('catedra')
+        if catedra is not None:
+            queryset = queryset.filter(catedra=catedra)
+        return queryset
