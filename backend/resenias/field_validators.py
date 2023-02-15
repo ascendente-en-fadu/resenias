959f6a40e0170caf9b7e606bb54f4dd7bb4a06b1
dev-guido <guidovernarusso@gmail.com>
2023-02-15 19:53:30 -0300

    Se cambia el anio minimo de la cursada a dos mil

--- backend/resenias/field_validators.py
+++ backend/resenias/field_validators.py
@@ -1,11 +1,11 @@
 """Validators para los models fields de la aplicacion resenias"""
 import datetime
 from django.core.exceptions import ValidationError
-ANIO_MINIMO = 2010
+ANIO_MINIMO = 2000
 
 
 def validar_anio(value):
-    """Valida que el anio este entre 2010 y el anio actual (incluidos)."""
+    """Valida que el anio este entre 2000 y el anio actual (incluidos)."""
     if value > datetime.date.today().year:
         raise ValidationError(
             message="No puede ingresar un año que aún no haya transcurrido"
