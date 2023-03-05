ff9d397d194fce1418ee0ef34b33af0ca138d7d5
dev-guido <guidovernarusso@gmail.com>
2023-03-04 22:23:39 -0300

    volvemos a incluir la resenia propia en el listado gral

--- backend/resenias/views.py
+++ backend/resenias/views.py
@@ -79,24 +79,6 @@ class ReseniaView(viewsets.ModelViewSet):
             queryset = queryset.filter(catedra=catedra)
         return queryset
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        # autor
-        encryption_key = config('FERNET_KEY')
-        fernet = Fernet(encryption_key)
-        encrypted_email = self.request.META.get('HTTP_SESSION_ID')
-        if encrypted_email:
-            email = fernet.decrypt(encrypted_email).decode()
-            queryset = queryset.exclude(autor=email)
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
