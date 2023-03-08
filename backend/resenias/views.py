8d0c2c8eb046eeec7b4ef1c38d800e8c9446b234
dev-guido <guidovernarusso@gmail.com>
2023-03-07 21:37:04 -0300

    agrego validaciones a la edicion de resenia

--- backend/resenias/views.py
+++ backend/resenias/views.py
@@ -97,6 +97,30 @@ class ReseniaView(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        encrypted_email = request.META.get('HTTP_SESSION_ID')
+        if not encrypted_email:
+            error_msge = 'No se puede editar una reseña sin iniciar sesión.'
+            return Response(error_msge, status=status.HTTP_401_UNAUTHORIZED)
+        encryption_key = config('FERNET_KEY')
+        fernet = Fernet(encryption_key)
+        email = fernet.decrypt(encrypted_email).decode()
+        if instance.autor != email:
+            error_msge = 'No se puede editar una reseña ajena.'
+            return Response(error_msge, status=status.HTTP_403_FORBIDDEN)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         encrypted_email = request.META.get('HTTP_SESSION_ID')
