aac7b6206e9b9447f2a30d4723ba622a8fb042ab
luqas11 <lucascazas@hotmail.com>
2023-03-15 11:07:10 -0300

    Add character limit to review input

--- frontend/src/constants/misc.js
+++ frontend/src/constants/misc.js
@@ -1,5 +1,6 @@
 export const REVIEW_YEARS_MAX_COUNT = 5;
 export const VALID_YEAR_MONTH_LIMIT = 2;
+export const MAX_REVIEW_INPUT_CHARS = 2500;
 
 export const RATES = [
   { id: 200000, name: 'Sobre nivel' },
