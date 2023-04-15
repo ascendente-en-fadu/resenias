4866154f2f77f3b63446ff83e46985c632c6861d
luqas11 <lucascazas@hotmail.com>
2023-04-15 00:03:26 -0300

    Implement new mock validation logic

---
+++ frontend/mock_server.js
@@ -0,0 +1,92 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+/**
+ * Function to get current user's own review, selected by course or review id
+ */
+const getMyReview = ({ catedra, id }) => {
+  const data = router.db
+    .get('resenias')
+    .value()
+    .find(
+      (resenia) =>
+        (resenia.catedra === Number(catedra) || resenia.id === Number(id)) &&
+        resenia.autor === 'own',
+    );
+  if (data) {
+    // eslint-disable-next-line no-unused-vars
+    const { autor, ..._data } = data;
+    return _data;
+  }
+};
+
+/**
+ * Validates a review object properties, checking if it matches the review definitions
+ */
+const isValidReview = (review) => {
+  return (
+    review.anio >= 2000 &&
+    review.anio < new Date().getFullYear() &&
+    review.contenido &&
+    review.contenido.length <= 2500
+  );
+};
+
+/**
+ * Middleware for the own review endpoint, that returns the current user's review
+ */
+const ownReviewMiddleware = (req, res) => {
+  const ownReview = getMyReview({ catedra: req.query.catedra });
+  res.json(ownReview ?? {});
+};
+
+/**
+ * Middleware for the login endpoint, that returns a generic session_id
+ */
+const loginMiddleware = (req, res) => {
+  res.json({
+    session_id: 'some-session-id',
+  });
+};
+
+/**
+ * Middleware for the create and delete reviews endpoints, that executes some validation logic
+ */
+const reviewsMiddleware = (req, res, next) => {
+  switch (req.method) {
+    case 'POST':
+      if (
+        !getMyReview({ catedra: req.body.catedra }) &&
+        isValidReview(req.body)
+      ) {
+        req.body.autor = 'own';
+        next();
+      } else {
+        res.sendStatus(403);
+      }
+      break;
+    case 'DELETE':
+      if (getMyReview({ id: req.path.replaceAll('/', '') })) {
+        next();
+      } else {
+        res.sendStatus(403);
+      }
+      break;
+    default:
+      next();
+  }
+};
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use('/api/resenia-propia', ownReviewMiddleware);
+server.use('/api/login', loginMiddleware);
+server.use('/api/resenias', reviewsMiddleware);
+server.use('/api', router);
+
+server.listen(3004, () => {
+  console.log('JSON Server is running');
+});
